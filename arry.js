93fefac32b96df8f11b0470d390fc3cec9de346a
sahabulislamsifat <sahabulislamsifat@gmail.com>
2024-09-05 09:06:04 +0600

    first commit

---
+++ arry.js
@@ -0,0 +1,17 @@
+// const num1 = 1;
+// const num2 = 2;
+// const num3 = 3;
+// const num4 = 4;
+// const num5 = 5;
+// const num6 = 6;
+// const num7 = 7;
+// const num8 = 8;
+
+// const numbers = 123456987910111;
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// console.log(numbers)
+
+// const friends = ["Abul, Babul, Kabil, Sabul, Mabul, etc."];
+// const mixedArry = [124351, "Rafique, Rakib, Rajib" 234.20.360];
+
